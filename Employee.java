b4658c6c3fe9b2835a613b26822f7a5a1d387cf9
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-22 09:39:32 +0300

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,56 @@
+public class Employee {
+    String name;
+    int salary;
+    int workHours;
+    int hireYear;
+
+    Employee(String name, int salary, int workHours, int hireYear){
+        this.name=name;
+        this.salary=salary;
+        this.workHours=workHours;
+        this.hireYear=hireYear;
+    }
+
+    double tax(){
+        if (this.salary <1000){
+            return 0;
+        }
+        else
+            return salary * 0.03;
+    }
+    int bonus() {
+        int overWork=0;
+        if (this.workHours>40){
+            overWork=workHours-40;
+            return overWork=overWork*30;
+        }
+        else
+           return overWork=0;
+
+
+    }
+    double raiseSalary(){
+        int year = 2021, totalYear;
+        totalYear=year - hireYear;
+        if (totalYear<10){
+          return this.salary*0.05;    
+        } else if (totalYear>=10 && totalYear<20) {
+            return this.salary*0.1;
+            }
+        else
+            return salary*0.15;
+    }
+    void run(){
+        System.out.println("Çalışanın Adı :" + this.name);
+        System.out.println("Maaşı :" + this.salary);
+        System.out.println("Çalışma Saati :" + this.workHours);
+        System.out.println("Başlangıc Yılı : " + this.hireYear);
+        System.out.println("Maaşınızın vergisi :" + tax());
+        System.out.println("Bonus :" + bonus());
+        double raiseSalary=raiseSalary();
+        System.out.println("Maaş Artışı :" + raiseSalary);
+        double totalSalaryWT = salary + bonus() - tax() ;
+            System.out.println("Vergi ve Bonuslarla Maaş :" + totalSalaryWT);
+        System.out.println("Toplam Maaş :" + (salary + raiseSalary));
+    }
+}
